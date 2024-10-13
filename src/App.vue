973c90346635ebfb8d187032a5753e0ee31c859b
Mariam <kelanimaryam1234@gmail.com>
2024-10-13 04:53:48 +0100

    App.vue file for sidebar and a router-view codes inserted

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Header />
+    <div class="main-container">
+      <Sidebar />
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Sidebar from "./components/Sidebar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Sidebar,
+  },
+};
+</script>
+
+<style scoped>
+.main-container {
+  display: flex;
+}
+</style>
