405820a840e72ab5332345684676dc67362f460d
Mariam <kelanimaryam1234@gmail.com>
2024-10-13 07:27:25 +0100

    header codes and styles defined

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Header />
+    <div class="main-container">
+      <AppSidebar />
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "./components/Header.vue";
+import AppSidebar from "./components/AppSidebar.vue";
+
+export default defineComponent({
+  components: {
+    Header,
+    AppSidebar,
+  },
+});
+</script>
+
+<style scoped>
+.main-container {
+  display: flex;
+}
+</style>
