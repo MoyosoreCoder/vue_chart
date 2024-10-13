24f1d7e7c4b1d82f809f5cf478b2e2b89221543a
Mariam <kelanimaryam1234@gmail.com>
2024-10-13 04:57:05 +0100

    code for header layout of the website

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>Dashboard Analytics</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+header {
+  height: 60px;
+  background-color: #34495e;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
