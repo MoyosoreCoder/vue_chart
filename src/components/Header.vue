47a3323654227bdd344ed9c93608440fc4e5857e
Mariam <kelanimaryam1234@gmail.com>
2024-10-13 07:28:14 +0100

    side bar codes adding two links for now

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="app-header">
+    <h1>Dashboard</h1>
+  </header>
+</template>
+
+<style scoped>
+.app-header {
+  background-color: #3c3c3c;
+  color: white;
+  padding: 1rem;
+}
+</style>
