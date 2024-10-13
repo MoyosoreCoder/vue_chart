fb299eadc9f6803e375609bafe6a0990dcccf191
Mariam <kelanimaryam1234@gmail.com>
2024-10-13 07:29:40 +0100

    main scss styles inserted

---
+++ src/components/BarChart.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <Bar :chart-data="chartData" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
+export default defineComponent({
+  components: {
+    Bar,
+  },
+  data() {
+    return {
+      chartData: {
+        labels: ["January", "February", "March"],
+        datasets: [
+          {
+            label: "Sales",
+            backgroundColor: "#42A5F5",
+            data: [40, 20, 12],
+          },
+        ],
+      },
+    };
+  },
+});
+</script>
+
+<style scoped>
+/* Add styles specific to your chart here */
+</style>
