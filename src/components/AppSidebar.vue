5558a79d7b60429ca0ef897c0cd833ca33048513
Mariam <kelanimaryam1234@gmail.com>
2024-10-13 07:28:57 +0100

    bar chart graph using chart js

---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <aside class="app-sidebar">
+    <ul>
+      <li><router-link to="/">Dashboard</router-link></li>
+      <li><router-link to="/settings">Settings</router-link></li>
+      <li><router-link to="/reports">Reports</router-link></li>
+    </ul>
+  </aside>
+</template>
+
+<style scoped>
+.app-sidebar {
+  width: 200px;
+  background-color: #f4f4f4;
+  padding: 1rem;
+}
+</style>
